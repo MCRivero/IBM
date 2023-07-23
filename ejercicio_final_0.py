abc5668e920ab9bc07c2aa6bbe60ac52b0edeb86
MCRivero <carmen.rivero.perez7@gmail.com>
2023-07-23 21:04:50 +0200

    v4.0

--- ejercicio_final_0.py
+++ ejercicio_final_0.py
@@ -3,36 +3,35 @@ Escribir un programa en Python que genere una matriz de tamaño NxN y la llene c
 El programa deberá imprimir la matriz generada y luego calcular la suma de los elementos de cada fila y columna. 
 Finalmente, deberá imprimir la suma de cada fila y columna.""" 
 
-import random
-
 # 1. Generar una matriz cuadrada de tamaño NXN. N es un numero entero ingresado por el usuario
-matriz=[]
+n_filas = 0
+n_columnas = 0
+lista=[]
 
-N = int(input("Introduzca el tamaño de la matriz: "))
+n_tamaño_matriz = int(input("Introduzca el tamaño de la matriz: "))
 
+print('Filas:', n_tamaño_matriz, 'columnas:', n_tamaño_matriz)
+print(type(n_tamaño_matriz))
+print('n_filas',n_filas)
+print('n_columnas',n_columnas)
+
+print('----------------------------')
 
 # 2. Rellenar la matriz con numeros aleatorios entre 0 y 9.
 
-for i in range(N):
-    fila = []
-    for j in range(N):
-        fila.append(random.randint(0,9))
-    matriz.append(fila)
+while (n_filas < n_tamaño_matriz):
+    while (n_columnas < n_tamaño_matriz):
+#        lista[n_filas],[n_columnas] = 1
+#        print(lista[n_filas],[n_columnas])
+        print('Dentro de columnas', lista)
+        n_columnas += 1
+    print('Dentro de filas', lista)
+    n_filas += 1
 
 # 3. Imprimir la matriz
-for fila in matriz:
-    print(fila)
+
 
 # 4. Calcular la suma de cada fila y columna
-suma_filas = []
-suma_columnas = [0] * len(matriz)
 
-for fila in matriz:
-    suma_fila = sum(fila)
-    suma_filas.append(suma_fila)
-    for j, elemento in enumerate(fila):
-        suma_columnas[j] += elemento    
 
 # 5. Imprimir la suma de cada fila y columna
-print('suma filas:', suma_filas)
-print('suma_columnas:', suma_columnas)
